d7301692734ea8060c22d3c12699c044c4347ca8
Pavan Netha <Chilukuripavan4185@gmail.com>
2026-08-01 20:53:27 +0530

    vercel config code

--- server/server.js
+++ server/server.js
@@ -24,7 +24,12 @@ import { Readable, Transform } from "stream";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 mongoose
